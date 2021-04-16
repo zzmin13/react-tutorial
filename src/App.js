8900b4ac8443753388aaccbda445f32d238c3a86
zzmin13 <zzzz1997@naver.com>
2021-04-16 12:37:26 +0900

    JSX 문법

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
 import Hello from "./Hello";
+import "./App.css";
 
 function App() {
+  const name = 'react';
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: 24,
+    padding: '1rem'
+  }
   return (
-    <div>
-      <Hello />
-    </div>
+    <>
+      {/* 어쩌고 저쩌고 */}
+      <Hello 
+      // 이 내용은 보이지 않습니다.
+      />
+      <div style={style}>{name}</div>
+      <div className="gray-box"></div>
+    </>
   );
 }
 
