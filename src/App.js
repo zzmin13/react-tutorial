d09cd8cd1806ff8a2b98fd1da9ff4774f3c2ccea
zzmin13 <zzzz1997@naver.com>
2021-04-16 13:05:33 +0900

    props와 children

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from "react";
 import Hello from "./Hello";
+import Wrapper from './Wrapper';
 import "./App.css";
 
 function App() {
-  const name = 'react';
-  const style = {
-    backgroundColor: 'black',
-    color: 'aqua',
-    fontSize: 24,
-    padding: '1rem'
-  }
   return (
-    <>
-      {/* 어쩌고 저쩌고 */}
-      <Hello 
-      // 이 내용은 보이지 않습니다.
-      />
-      <div style={style}>{name}</div>
-      <div className="gray-box"></div>
-    </>
+    <Wrapper>
+      <Hello name="react" color="red"/>
+      <Hello color="blue"/>
+    </Wrapper>
   );
 }
 
--- src/Hello.js
+++ src/Hello.js
@@ -1,7 +1,11 @@
 import React from "react";
 
-function Hello(){
-    return <div>Hello!</div>;
+function Hello({name,color}){
+    return <div style={{color}}>{name}님 안녕하세요.</div>;
 }
+
+Hello.defaultProps = {
+    name: 'Unknown'
+};
 
 export default Hello;
