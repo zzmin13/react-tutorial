779a3a2868cefc1c2c2e10af722c13f2130c3cdc
zzmin13 <zzzz1997@naver.com>
2021-06-07 15:25:31 +0900

    useReducer - 보충

--- src/App.js
+++ src/App.js
@@ -1,76 +1,123 @@
-import React, {useState , useRef, useMemo, useCallback} from "react";
+import React, { useReducer, useRef, useMemo, useCallback } from "react";
 import "./App.css";
-import CreateUser from './CreateUser';
-import UserList from './UserList';
+import CreateUser from "./CreateUser";
+import UserList from "./UserList";
 
-function countActiveUsers(users){
+function countActiveUsers(users) {
   return users.filter((user) => user.active).length;
-};
-
-function App() {
-  const [inputs, setInputs] = useState({
-    username: '',
-    email : ''
-  });
-  const {username, email} = inputs;
-  const [users, setUsers] = useState([
+}
+const initialState = {
+  inputs: {
+    username: "",
+    email: "",
+  },
+  users: [
     {
       id: 1,
-      username: 'velopert',
-      email: 'public.velopert@gmail.com',
-      active: false
+      username: "velopert",
+      email: "public.velopert@gmail.com",
+      active: false,
     },
     {
       id: 2,
-      username: 'tester',
-      email: 'tester@example.com',
-      active: false
+      username: "tester",
+      email: "tester@example.com",
+      active: false,
     },
     {
       id: 3,
-      username: 'liz',
-      email: 'liz@example.com',
-      active: false
-    }
-  ]);
+      username: "liz",
+      email: "liz@example.com",
+      active: false,
+    },
+  ],
+};
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "CHANGE_INPUT":
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.name]: action.value,
+        },
+      };
+    case "CREATE_USER":
+      return {
+        inputs: initialState.inputs,
+        users: state.users.concat(action.user),
+      };
+    case "REMOVE_USER":
+      return {
+        ...state,
+        users: state.users.filter((user) => user.id !== action.id),
+      };
+    case "TOGGLE_USER":
+      return {
+        ...state,
+        users: state.users.map((user) =>
+          user.id === action.id ? { ...user, active: !user.active } : user
+        ),
+      };
+    default:
+      throw new Error("Unhandled action");
+  }
+};
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
   const nextId = useRef(4);
-  
+  const {
+    inputs: { username, email },
+    users,
+  } = state;
+
   const onChange = useCallback((event) => {
-    const {name, value} = event.target;
-    setInputs({
-      ...inputs,
-      [name] : value
+    const name = event.target.name;
+    const value = event.target.value;
+    dispatch({
+      type: "CHANGE_INPUT",
+      name,
+      value,
     });
-  },[inputs]);
+  }, []);
 
   const onCreate = useCallback(() => {
-    const user = {
-      id : nextId.current,
-      username : username,
-      email : email
-    }
-    setUsers(users => users.concat(user));
+    dispatch({
+      type: "CREATE_USER",
+      user: {
+        id: nextId.current,
+        username,
+        email,
+      },
+    });
     nextId.current += 1;
-    setInputs({
-      username: '',
-      email: ''
-    })
   }, [username, email]);
 
   const onRemove = useCallback((id) => {
-    setUsers(users => users.filter((user) => user.id !== id));
-  },[]);
-  
+    dispatch({
+      type: "REMOVE_USER",
+      id,
+    });
+  }, []);
+
   const onToggle = useCallback((id) => {
-    setUsers(users => users.map((user) => user.id === id ? {...user, active: !user.active} : user));
-  },[]);
+    dispatch({
+      type: "TOGGLE_USER",
+      id,
+    });
+  }, []);
 
   const count = useMemo(() => countActiveUsers(users), [users]);
 
   return (
     <>
-      <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate}/>
-      <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
       <div>활성 사용자 수 : {count} </div>
     </>
   );
--- src/CreateUser.js
+++ src/CreateUser.js
@@ -1,23 +1,23 @@
 import React from "react";
 
-function CreateUser({username, email, onChange, onCreate}){
-    return(
-        <div>
-            {console.log(`CreateUser 컴포넌트가 렌더링 되었습니다.`)}
-            <input
-            name="username"
-            onChange={onChange}
-            placeholder="이름"
-            value={username}
-            />
-            <input
-            name="email"
-            onChange={onChange}
-            placeholder="이메일"
-            value={email}
-            />
-            <button onClick={onCreate}>등록</button>
-        </div>
-    )
+function CreateUser({ username, email, onChange, onCreate }) {
+  return (
+    <div>
+      {console.log(`CreateUser 컴포넌트가 렌더링 되었습니다.`)}
+      <input
+        name="username"
+        onChange={onChange}
+        placeholder="이름"
+        value={username}
+      />
+      <input
+        name="email"
+        onChange={onChange}
+        placeholder="이메일"
+        value={email}
+      />
+      <button onClick={onCreate}>등록</button>
+    </div>
+  );
 }
 export default React.memo(CreateUser);
