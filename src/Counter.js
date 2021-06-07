15a1b213eda91da4fb059530350cf303959154f3
zzmin13 <zzzz1997@naver.com>
2021-06-07 14:07:55 +0900

    useReducer - 기초

--- src/Counter.js
+++ src/Counter.js
@@ -1,22 +1,35 @@
-import React from "react";
-import {useState} from "react";
+import React, { useReducer } from "react";
+import { useState } from "react";
 
-function Counter(){
-    const [number, setNumber] = useState(0);
-    const [string] = useState('hi');
-    const onIncrease = () => {
-        setNumber(prev => prev + 1);
-    };
-    const onDecrease = () => {
-        setNumber(prev => prev - 1);
-    };
-    return (
-        <div>
-            <h1>{number}{string}</h1>
-            <button onClick={onIncrease}>+1</button>
-            <button onClick={onDecrease}>-1</button>
-        </div>
-    )
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    default:
+      throw new Error("Unhandled action");
+  }
+};
+function Counter() {
+  const [number, dispatch] = useReducer(reducer, 0);
+  const onIncrease = () => {
+    dispatch({
+      type: "INCREMENT",
+    });
+  };
+  const onDecrease = () => {
+    dispatch({
+      type: "DECREMENT",
+    });
+  };
+  return (
+    <div>
+      <h1>{number}</h1>
+      <button onClick={onIncrease}>+1</button>
+      <button onClick={onDecrease}>-1</button>
+    </div>
+  );
 }
 
 export default Counter;
